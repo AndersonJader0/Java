c84869196cb21babe6520aa34351759c873294d1
andersonjader <andersonjader036@gmail.com>
2022-10-06 23:34:21 -0200

    Update FormulaCasamento.java

--- Faculdade/FormulaCasamento.java
+++ Faculdade/FormulaCasamento.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class FormulaCasamento {
     public static void main (String[] args){
 
-        int idade, n, p, calculo; /* "n" = idade máxima e "p" = idade mínima. */
+        int age, maxAge, minAge, calculate; /* "n" = idade máxima e "p" = idade mínima. */
 
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Esse é um programinha para calcular a idade ideal para se casar!"
         + "\nDigite a sua idade: "); 
-        idade = entrada.nextInt();
+        age = entrada.nextInt();
 
         System.out.println("Estabeleça o limite de idade em que você quer se casar: "); 
-        n = entrada.nextInt(); // Idade máxima.
+        maxAge = entrada.nextInt(); // Idade máxima.
 
         System.out.println("Pense na menor idade que você se cogitaria subir no altar: ");
-        p = entrada.nextInt(); // Idade mínima.
+        minAge = entrada.nextInt(); // Idade mínima.
 
-        calculo = (int) ((((n - p) * 0.368) + p));
+        calculate = (int) ((((maxAge - minAge) * 0.368) + minAge));
 
         System.out.println("A idade ideal para se casar é " + calculo);
 
-        if (calculo == idade){
+        if (calculate == age){
             System.out.print("Já pode comprar o anel e preparar do discurso!");
-        }else if (calculo > idade){
+        }else if (calculate > age){
             System.out.print("Ainda está cedo!");
         } else{
             System.out.print("Já passou da época!");
