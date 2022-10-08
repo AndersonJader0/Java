a8f3b920dc4f077f91a63ea54f1cbeff26d6905e
AndersonJader0 <andersonjader036@gmail.com>
2022-10-08 15:58:28 -0300

    Changed the calculate to projects

--- Projects/src/Calculate.java
+++ Projects/src/Calculate.java
@@ -1,17 +1,18 @@
-package Projetos;
 import javax.swing.JOptionPane;
 
-public class Calculadora {
+public class Calculate {
     public static void main(String[] args) {
 
-        Float answer, number1, number2, result;
+        float answer, number1, number2, result;
 
-        JOptionPane.showMessageDialog(null, "This is a simple calculator! Your base is with float! And the next you will see the options of calculations");
-        answer = Float.parseFloat(JOptionPane.showInputDialog("What calculation would you like to?" //O JOptionPane trabalha apenas com variáveis do tipo String por isso a utilização do parse.
-        + "\n1 - Addition"
-        + "\n2 - Subtraction"
-        + "\n3 - Multiplication"
-        + "\n4 - Division"
+        JOptionPane.showMessageDialog(null, "This is a simple calculator! the next you will see the options of calculations");
+        //O JOptionPane trabalha apenas com variáveis do tipo String por isso a utilização do parse.
+        answer = Float.parseFloat(JOptionPane.showInputDialog("""
+                What calculation would you like to?
+                1 - Addition
+                2 - Subtraction
+                3 - Multiplication
+                4 - Division"""
         ));
 
         if (answer == 1) {
@@ -21,33 +22,29 @@ public class Calculadora {
             result = number1 + number2;
 
             JOptionPane.showMessageDialog(null, "The Addition resul = " + result);
-        }
-
-        else if (answer == 2) {
+        } else if (answer == 2) {
             number1 = Float.parseFloat(JOptionPane.showInputDialog("What the Minuend?"));
             number2 = Float.parseFloat(JOptionPane.showInputDialog("What the subtrahend?"));
 
             result = number1 - number2;
 
             JOptionPane.showMessageDialog(null, "The Subtraction result = " + result);
-        }
-
-        else if (answer == 3) {
+        } else if (answer == 3) {
             number1 = Float.parseFloat(JOptionPane.showInputDialog("What the first number?"));
             number2 = Float.parseFloat(JOptionPane.showInputDialog("What the second number?"));
 
             result = number1 * number2;
 
             JOptionPane.showMessageDialog(null, "The Multiplication result = " + result);
-        }
-
-        else if (answer == 4) {
+        } else if (answer == 4) {
             number1 = Float.parseFloat(JOptionPane.showInputDialog("What the dividend?"));
             number2 = Float.parseFloat(JOptionPane.showInputDialog("What the divisor?"));
 
             result = number1 / number2;
 
-            JOptionPane.showMessageDialog(null, "The Divison result = " + result);
+            JOptionPane.showMessageDialog(null, "The Division result = " + result);
+        } else {
+            JOptionPane.showMessageDialog(null, "We cannot calculate this");
         }
     }
 }
--- Projects/src/Imc.java
+++ Projects/src/Imc.java
@@ -1,14 +1,48 @@
 import javax.swing.JOptionPane;
 
 public class Imc {
-    public static void main(String[] args){
-        Float heigth, weight, imc;
+    public static void main(String[] args) {
 
-        heigth = Float.parseFloat(JOptionPane.showInputDialog("Digite a altura"));
-        weight = Float.parseFloat(JOptionPane.showInputDialog("Digite o peso"));
+        String Name, Sex;
+        int age;
+        double height, weight, imc;
 
-        imc = weight /(heigth*2);
+        JOptionPane.showMessageDialog(null, "Esse é um programinha para calcular o IMC - (Índice de Massa Corporal).");
 
-        JOptionPane.showMessageDialog(null, "O IMC é = " + imc);
+        Name = JOptionPane.showInputDialog("Digite o nome");
+        Sex = JOptionPane.showInputDialog("Digite o sexo");
+        age = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade"));
+        height = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura em metros"));
+        weight = Double.parseDouble(JOptionPane.showInputDialog("Digite o peso em kg"));
+
+        imc = weight/(height*2);
+
+        if (imc <= 18.5) {
+
+            JOptionPane.showMessageDialog(null, "Dados"
+                    + "\nNome: " + Name
+                    + "\nSexo: " + Sex
+                    + "\nIdade: " + age
+                    + "\nIMC: " + imc
+                    + "\nClassificação: Magreza"
+            );
+        }else if (imc >= 25) {
+
+            JOptionPane.showMessageDialog(null, "Dados"
+                    + "\nNome: " + Name
+                    + "\nSexo: " + Sex
+                    + "\nIdade: " + age
+                    + "\nIMC: " + imc
+                    + "\nClassificação: Obesidade"
+            );
+        }else {
+            JOptionPane.showMessageDialog(null, "Dados"
+                    + "\nNome: " + Name
+                    + "\nSexo: " + Sex
+                    + "\nIdade: " + age
+                    + "\nIMC: " + imc
+                    + "\nClassificação: Normal"
+            );
+        }
     }
 }
